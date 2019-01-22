bd13b4ff9ab229c444b12316277082dbae4f8010
Brian Illes <brianrilles@gmail.com>
2019-01-22 12:26:54 -0800

    completed callbacks.js normal challenges

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -28,25 +28,46 @@ function getLength(arr, cb) {
 }
 
 getLength(items, function(length) {
-  console.log(length);
+  console.log(length)
 });
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(last) {
+  console.log(last)
+});
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(2, 4, function(add) {
+  console.log(add)
+}); 
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(3, 3, function(multiply) {
+  console.log(multiply)
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+
+contains("yo-yo", items, function(inside) {
+  console.log(inside)
+});
 
 /* STRETCH PROBLEM */
 

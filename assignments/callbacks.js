546a6435afc343742221454230187a538eab6edd
Brian Illes <brianrilles@gmail.com>
2019-01-22 20:39:53 -0800

    added stretch problem on callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum", "Gum"];
 
 /* 
 
@@ -82,7 +82,16 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  return cb();
+  var newArray = [];
+  for (i in array) {
+    if (!newArray.includes(array[i])) {
+      newArray.push(array[i])
+      //debugger;
+    }
+  }
+  return cb(newArray);
 }
-
-
+//works for items too
+removeDuplicates([1, 1, 2, 3, 4, 5, 6, 6, 7], function(inside) {
+  console.log(inside);
+});
